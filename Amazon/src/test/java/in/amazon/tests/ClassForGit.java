00735fe8184f1f8801440c59c4ddd279a9c2a927
sanke <sanke@LAPTOP-NQFC7GRV.lan>
2024-10-19 23:50:19 +0530

    Commit For Git PR new

--- Amazon/src/test/java/in/amazon/tests/ClassForGit.java
+++ Amazon/src/test/java/in/amazon/tests/ClassForGit.java
@@ -4,6 +4,7 @@ public class ClassForGit {
 
 	public static void main(String[] args) {
 		System.out.println("we are here to solve your complex probs");
+		System.out.println("njdnuiwdsnjwdnjckdn");
 	}
 
 }
